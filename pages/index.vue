040eb0d44562b1098427e797fd8813b90f460b01
Vincent <courtois.vincent@outlook.com>
2023-11-30 16:56:50 +0100

    Finish the application for the thrid directed class

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,30 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="8">
-        <v-textarea variant="outlined" no-resize rows="15" bg-color="white" label="Requête" placeholder="db.collection...." v-model="query" />
-        <v-btn color="primary" @click="validate">Valider</v-btn>
-      </v-col>
-      <v-col cols="4">
-        <v-alert type="info" title="structure">
-          <pre>
-            {
-              "_id": &lt;ObjectId&gt;
-            }
-          </pre>
+      <v-col cols="8" offset="2">
+        <v-alert type="info" class="mb-5" closable>
+          <div>Entrez le code correspondant à votre requête dans la boite de texte ci-dessous.</div>
+          <div>Cliquez ensuite sur "Valider".</div>
+          <div>Les résultats devraient s'afficher dans un tableau sous la boite de texte si tout s'est bien passé.</div>
+          <div><b>ATTENTION</b> N'oubliez pas d'ajouter un return explicite comme dans l'exemple donné pour renvoyer un résultat.</div>
         </v-alert>
+        <!--v-textarea variant="outlined" no-resize rows="15" bg-color="white" label="Requête" placeholder="db.collection...." v-model="query" /-->
+        <HighCode
+          class="code"
+          :codeValue="query"
+          width="100%"
+          height="500px"
+          lang="javascript"
+          theme="light"
+          :textEditor="true"
+          ref="query"
+        ></HighCode>
+        <v-btn color="primary" class="mt-5" @click="validate">Valider</v-btn>
       </v-col>
       <!-- The list of results-->
-      <v-col cols="12" v-if="results">
+      <v-col cols="8" offset="2" v-if="results">
         <!-- In the case the results are an array, it dynamically draws it -->
-        <v-table v-if="(results instanceof Array)">
-          <thead>
-            <tr>
-              <th v-for="(_, index) in results[0]">{{ index }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="result in results">
-              <td v-for="(_, index) in results[0]">{{ result[index] }}</td>
-            </tr>
-          </tbody>
-        </v-table>
-        <!-- This will mostly happen in the case of aggregations -->
+        <v-data-table v-if="(results instanceof Array)" :items="results" />
         <pre v-else>{{ results }}</pre>
       </v-col>
     </v-row>
@@ -38,12 +33,27 @@
 
 <script setup>
 import axios from 'axios';
+import { HighCode } from 'vue-highlight-code';
+import 'vue-highlight-code/dist/style.css';
 
-const query = ref('');
+const query = ref('return db.books.find({}, {title: 1}).limit(10);');
+
+onMounted(() => {
+  const stored = localStorage.getItem("sandbox-code")
+  if (!!stored) query.value.modelValue = stored;
+})
 
 const results = ref(false);
 
 async function validate() {
-  results.value = (await axios.post("/queries", {query: query.value})).data;
+  localStorage.setItem("sandbox-code", query.value.modelValue);
+  results.value = (await axios.post("/queries", {query: query.value.modelValue})).data;
+  console.log(results.value);
 }
 </script>
+
+<style scoped>
+.wrapped {
+  white-space: nowrap
+}
+</style>
--- server/routes/queries.ts
+++ server/routes/queries.ts
@@ -1,32 +1,42 @@
-import mongoose from "mongoose";
-
-const conn = mongoose.connection;
-
-const AsyncFunction = (async function () {}).constructor;
-
-const handler = {
-  get(cible, prop, recepteur) {
-    return cible.collection(prop);
-  }
-}
+import * as child_process from "child_process";
+import { promises as fs } from 'fs'
 
 export default defineEventHandler(async (event) => {
 
-  let connection;
-
   try {
-    const connection = await mongoose.connect('mongodb://localhost:27017/iut');
-    const body = await readBody(event);
-    const db = new Proxy(connection.connections[0], handler);
-    
-    const fct = AsyncFunction('db', body.query);
 
-    const results = await fct(db);
+    const uri = process.env.MONGODB_URL
 
-    console.log(results);
+    const filename = `${(new Date()).getTime()}`;
 
-    return body.query;
-  } catch (err) {
-    console.error("DB connection failed.", err);
+    const queryFile = `./tmp/query_${filename}.js`;
+    const resultsFile = `tmp/results_${filename}.json`
+
+    const { query } = await readBody(event);
+    // Synchronous writing of the file to ensure data are available for the command line after.
+    await fs.writeFile(queryFile, `function query() { ${query} }; print_value(query());`);
+
+    function pr(): Promise<any> {
+        return new Promise(async function (resolve, reject) {
+        child_process.exec(`mongosh --quiet ${uri} ./tmp/functions.js ${queryFile} > ${resultsFile}`, (error, stdout, stderr) => {
+          if (error) reject(error);
+          if(stderr) reject(stderr);
+          else resolve(stdout);
+        });
+      });
+    }
+
+    await pr();
+
+    const results = await fs.readFile(resultsFile);
+
+    fs.unlink(queryFile);
+    fs.unlink(resultsFile);
+
+    return results
+  }
+  catch (err) {
+    console.log(err);
+    return {"error": "Une erreur est survenue lors de l'exécution de la requête"};
   }
 });
